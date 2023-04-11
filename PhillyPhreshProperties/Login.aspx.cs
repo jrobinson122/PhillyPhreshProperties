b4b89c3db8b533b1b205da6f3c415be1cd40cdc4
Joe Michel <tun69277@temple.edu>
2023-04-11 17:51:10 -0400

    Created User Class Fully added logic for Forgot Password

--- PhillyPhreshProperties/Login.aspx.cs
+++ PhillyPhreshProperties/Login.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using PhillyPhreshPropertiesLibrary;
 
 namespace PhillyPhreshProperties
 {
     public partial class Login : System.Web.UI.Page
     {
+        User user = new User();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,18 +19,61 @@ namespace PhillyPhreshProperties
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-
-        }
+            Response.Redirect("AccountRegistration.aspx");
+        }//end btnRegister_Click()
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
+            Validation();
+
+            if (!lblError.Visible)
+            {
+                user.GetUser();
+                Response.Redirect("LandingPage.aspx");
+            }
+
             //need a check for if chkSaveLoginInfo is checked
             //
-        }
+        }//end btnLogin_Click()
 
         protected void btnForgot_Click(object sender, EventArgs e)
         {
+            user.Email = txtEmail.Text;
 
+            if(txtEmail.Text == "")
+            {
+                InputError("Please enter your email above first then click Forgot Password again.");
+            }
+            else
+            {
+                lblError.Visible = false;
+                user.PasswordRecovery();
+
+                if (!user.PasswordRecovery())
+                {
+                    InputError("Your account was not found. Please check email for spelling errors and try agian.");
+                }
+                else
+                {
+                    SecurityQuestion();
+                }                
+            }
+
+        }//end btnForgot_Click()
+
+        protected void btnConfirmRecovery_Click(object sender, EventArgs e)
+        {
+            user.CheckSecurityAnswer(txtRecoveryAnswer.Text);
+
+            if (user.CheckSecurityAnswer(txtRecoveryAnswer.Text))
+            {
+                lblRecoveredPassword.Text = "Your password is " + user.Password;
+                lblError.Visible = false;
+            }
+            else
+            {
+                InputError("That answer is inccorect please try again.");
+            }
         }
 
         private void Validation()
@@ -46,12 +92,34 @@ namespace PhillyPhreshProperties
             }
         }//end Validation()
 
+        private void SecurityQuestion()
+        {
+            Random number = new Random();
+            int question = number.Next(1, 4); //generates random number between 1 and 3
+
+            switch (question)
+            {
+                case 1:
+                    lblRecoveyQuestion.Text = user.SecurityQuestion1;
+                    break;
+                case 2:
+                    lblRecoveyQuestion.Text = user.SecurityQuestion2;
+                    break;
+                case 3:
+                    lblRecoveyQuestion.Text = user.SecurityQuestion3;
+                    break;
+            }
+
+            lblRecoveyQuestion.Visible = true;
+            txtRecoveryAnswer.Visible = true;
+            btnConfirmRecovery.Visible = true;
+        }//end SecurityQuestion
+
         public void InputError(string error)
         {
             lblError.Text = error;
             lblError.Visible = true;
         }//end InputError()
 
-        
     }//end Login class
 }
---
+++ PhillyPhreshPropertiesLibrary/User.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace PhillyPhreshPropertiesLibrary
+{
+    public class User
+    {
+        private string userID;
+        private string email = "";
+        private string password = "";
+        private string firstname = "";
+        private string lastname = "";
+        private string address = "";
+        private string city = "";
+        private string state = "";
+        private int zipcode = 0;
+        private int phoneNumber = 0;
+        private string accountType = "";
+        private string securityQuestion1 = "";
+        private string answer1 = "";
+        private string securityQuestion2 = "";
+        private string answer2 = "";
+        private string securityQuestion3 = "";
+        private string answer3 = "";
+
+        //database connection
+        DBConnect db = new DBConnect();
+        SqlCommand objCommand = new SqlCommand();
+        DataSet dataset = new DataSet();
+
+        //default constructor
+        public User() { }
+
+        public User(string email, string password, string firstName, string lastName, string address, string city, string state, int zipcode, int phoneNumber, string accountType, string securityQuestion1, string answer1, string securityQuestion2, string answer2, string securityQuestion3, string answer3)
+        {
+            this.email = email;
+            this.password = password;
+            this.firstname = firstName;
+            this.lastname = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.phoneNumber = phoneNumber;
+            this.accountType = accountType;
+            this.securityQuestion1 = securityQuestion1;
+            this.answer1 = answer1;
+            this.securityQuestion2 = securityQuestion2;
+            this.answer2 = answer2;
+            this.securityQuestion3 = securityQuestion3;
+            this.answer3 = answer3;
+        }
+
+        //method returns true if user has been found and false if user is not found or error occurred
+        public bool GetUser()
+        {
+            /*
+             CREATE PROCEDURE [dbo].TP_GetUser
+	            @theEmail VARCHAR(MAX),
+	            @thePassword VARCHAR(MAX)
+            AS
+	            SELECT * FROM TP_User WHERE Email= @theEmail and Password= @thePassword
+            RETURN 0
+             */
+            try
+            {
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_GetUser";
+                objCommand.Parameters.AddWithValue("theEmail", email);
+                objCommand.Parameters.AddWithValue("thePassword", password);
+
+                dataset = db.GetDataSetUsingCmdObj(objCommand);
+                userID = dataset.Tables[0].Rows[0]["UserId"].ToString();
+                email= dataset.Tables[0].Rows[0]["Email"].ToString();
+                password= dataset.Tables[0].Rows[0]["Password"].ToString();
+                firstname= dataset.Tables[0].Rows[0]["FirstName"].ToString();
+                lastname= dataset.Tables[0].Rows[0]["LastName"].ToString();
+                //wait before finishing
+
+                objCommand.Parameters.Clear();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }// end GetUser()
+
+        public bool PasswordRecovery()
+        {
+            /*
+             CREATE PROCEDURE [dbo].TP_GetQuestions
+	            @theEmail VARCHAR(MAX)
+            AS
+	            SELECT * FROM TP_User WHERE Email= @theEmail
+            RETURN 0
+             */
+            try
+            {
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_ConfirmUser";
+                objCommand.Parameters.AddWithValue("theEmail", email);
+                
+                dataset = db.GetDataSetUsingCmdObj(objCommand);
+                securityQuestion1 = dataset.Tables[0].Rows[0]["SecurityQuestion1"].ToString();
+                answer1 = dataset.Tables[0].Rows[0]["Answer1"].ToString();
+                securityQuestion2 = dataset.Tables[0].Rows[0]["SecurityQuestion2"].ToString();
+                answer2 = dataset.Tables[0].Rows[0]["Answer2"].ToString();
+                securityQuestion3 = dataset.Tables[0].Rows[0]["SecurityQuestion3"].ToString();
+                answer3 = dataset.Tables[0].Rows[0]["Answer3"].ToString();
+                password = dataset.Tables[0].Rows[0]["Password"].ToString();
+
+
+                objCommand.Parameters.Clear();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }//end PasswordRecovery()
+
+        public bool CheckSecurityAnswer(string answer)
+        {
+
+
+            if (answer.CompareTo(answer1) == 0)
+            {
+                return true;
+            }
+            else if (answer.CompareTo(answer2) == 0)
+            {
+                return true;
+            }
+            else if (answer.CompareTo(answer3) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }//end CheckSecurityAnswer()
+
+        //getters and setters
+        public string UserID
+        { get { return userID; } set { userID = value; } }
+
+        public string Email
+        { get { return email; } set { email = value; } }
+
+        public string Password
+        { get { return password; } set { password = value; } }
+
+        public string FirstName
+        { get { return firstname; } set { firstname = value; } }
+
+        public string LastName
+        { get { return lastname; } set { lastname = value; } }
+
+        public string Address
+        { get { return address; } set { address = value; } }
+
+        public string City
+        { get { return city; } set { city = value; } }
+
+        public string State
+        { get { return state; } set { state = value; } }
+
+        public int Zipcode
+        { get { return zipcode; } set { zipcode = value; } }
+
+        public int PhoneNumber
+        { get { return phoneNumber; } set { phoneNumber = value; } }
+
+        public string AccountType
+        { get { return accountType; } set { accountType = value; } }
+
+        public string SecurityQuestion1
+        { get { return securityQuestion1; } set { securityQuestion1 = value; } }
+
+        public string Answer1
+        { get { return answer1; } set { answer1 = value; } }
+
+        public string SecurityQuestion2
+        { get { return securityQuestion2; } set { securityQuestion2 = value; } }
+
+        public string Answer2
+        { get { return answer2; } set { answer2 = value; } }
+
+        public string SecurityQuestion3
+        { get { return securityQuestion3; } set { securityQuestion3 = value; } }
+
+        public string Answer3
+        { get { return answer3; } set { answer3 = value; } }
+
+    }//end User class
+}
